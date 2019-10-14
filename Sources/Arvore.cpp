d2503531e3d01bb6bac8d033d97bdfb9d1760253
Samuel <samuelpedro1999@gmail.com>
2019-10-14 11:37:57 -0300

    Tentando debugar

--- Sources/Arvore.cpp
+++ Sources/Arvore.cpp
@@ -1,145 +1,5 @@
 #include "../Headers/Arvore.hpp"
 
-/*void Pesquisa(int Matriz[][3], Pont *p)
-{ if (*p == NULL)
-  { printf("Erro: Registro nao esta presente na arvore\n");
-    return;
-  }
-  if (x->Chave < (*p)->Reg.Chave)
-  { Pesquisa(x, &(*p)->Esq);
-    return;
-  }
-  if (x->Chave > (*p)->Reg.Chave)
-  Pesquisa(x, &(*p)->Dir);
-  else *x = (*p)->Reg;
-}*/
-
-/*void Insere(MatrizPuzzle MatrizP, Pont *p,VetorDeMexistentes *Lista, int* trava){
-  if(*trava>21000){
-    return;
-  }
-  *trava++;
-   if(*p == NULL){
-     *p = (Pont)malloc(sizeof(No));
-     for(int i=0;i<3;i++){
-       for(int j=0;j<3;j++){
-         (*p)->MatrizAtual[i][j]=MatrizP.Matriz[i][j];
-       }
-     }
-    (*p)->Esq = NULL;
-    (*p)->Dir = NULL;
-    (*p)->Cima = NULL;
-    (*p)->Baixo = NULL;
-    // return;
-  }
-  MatrizPuzzle MatrizPTeste;
-  MatrizesExistente MatrizE;
-  int Passe = 0;
-  //cout<<Lista->size()<<"-";
-
-  if(MatrizP.MovimentaBaixo(MatrizPTeste.Matriz)){
-
-        for(unsigned int k=0;k<Lista->Vetor.size();k++)
-          for(int i=0;i<3;i++)
-            for(int j=0;j<3;j++){
-              if(Lista->Vetor.at(k).Matriz[i][j]==MatrizPTeste.Matriz[i][j]){
-                //cout<<"Entrou false"<<endl;
-                Passe++;
-              }
-            }
-        if(Passe<9){
-          *trava++;
-          cout<<"Entrou Baixo"<<endl;
-
-          if(MatrizPTeste.VerificaResultado()){
-            cout<<endl<<"RESULTADO!";
-            return;
-          }
-          Insere(MatrizPTeste,&(*p)->Baixo,Lista,trava);
-          for(int i=0;i<3;i++)
-            for(int j=0;j<3;j++){
-              MatrizE.Matriz[i][j]=MatrizPTeste.Matriz[i][j];
-            }
-          Lista->Vetor.push_back(MatrizE);
-        }
-        Passe = 0;
-  }
-  if(MatrizP.MovimentaCima(MatrizPTeste.Matriz)){
-
-    for(unsigned int k=0;k<Lista->Vetor.size();k++)
-      for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++){
-          if(Lista->Vetor.at(k).Matriz[i][j]==MatrizPTeste.Matriz[i][j]){
-            //cout<<"Entrou false"<<endl;
-            Passe++;
-          }
-        }
-    if(Passe<9){
-      cout<<"Entrou cima"<<endl;
-      if(MatrizPTeste.VerificaResultado()){
-        cout<<endl<<"RESULTADO!";
-        return;
-      }
-      Insere(MatrizPTeste,&(*p)->Cima,Lista,trava);
-      for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++){
-          MatrizE.Matriz[i][j]=MatrizPTeste.Matriz[i][j];
-        }
-      Lista->Vetor.push_back(MatrizE);
-    }
-    Passe = 0;
-  }
-
-  if(MatrizP.MovimentaEsquerda(MatrizPTeste.Matriz)){
-
-    for(unsigned int k=0;k<Lista->Vetor.size();k++)
-      for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++){
-          if(Lista->Vetor.at(k).Matriz[i][j]==MatrizPTeste.Matriz[i][j]){
-            Passe++;
-          }
-        }
-    if(Passe<9){
-      cout<<"Entrou esquerda"<<endl;
-      if(MatrizPTeste.VerificaResultado()){
-        cout<<endl<<"RESULTADO!";
-        return;
-      }
-      Insere(MatrizPTeste,&(*p)->Esq,Lista,trava);
-      for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++){
-          MatrizE.Matriz[i][j]=MatrizPTeste.Matriz[i][j];}
-      Lista->Vetor.push_back(MatrizE);
-    }
-    Passe = 0;
-  }
-
-  if(MatrizP.MovimentaDireita(MatrizPTeste.Matriz)){
-
-    for(unsigned int k=0;k<Lista->Vetor.size();k++)
-      for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++){
-          if(Lista->Vetor.at(k).Matriz[i][j]==MatrizPTeste.Matriz[i][j]){
-            Passe++;
-          }
-        }
-    if(Passe<9){
-      cout<<"Entrou Direita"<<endl;
-      if(MatrizPTeste.VerificaResultado()){
-        cout<<endl<<"RESULTADO!";
-        return;
-      }
-      Insere(MatrizPTeste,&(*p)->Dir,Lista,trava);
-      for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++){
-          MatrizE.Matriz[i][j]=MatrizPTeste.Matriz[i][j];}
-     Lista->Vetor.push_back(MatrizE);
-    }
-    Passe = 0;
-  }
-
-}
-*/
 Arvore::Arvore(){
   this->Raiz = NULL;
 }
@@ -163,9 +23,9 @@ void Arvore::Insere(MatrizPuzzle MatrizP, Pont *p,Lista* Lista,int* trava){
    }
    MatrizPuzzle MatrizDeInsercao;
    if(MatrizP.MovimentaCima(MatrizDeInsercao.Matriz)){
-     cout<<"Movimento para Cima"<<endl;
      if(!Lista->existe(MatrizDeInsercao.Matriz)){
-       if(!MatrizP.VerificaResultado()){
+       cout<<"Movimento para Cima"<<endl;
+       if(!MatrizDeInsercao.VerificaResultado()){
          Lista->inserir_inicio(MatrizDeInsercao.Matriz);
        }
        else{
@@ -178,10 +38,10 @@ void Arvore::Insere(MatrizPuzzle MatrizP, Pont *p,Lista* Lista,int* trava){
        Insere(MatrizDeInsercao,&(*p)->Cima,Lista,trava);
      }
    }
-   if(MatrizP.MovimentaBaixo(MatrizDeInsercao.Matriz)){
-     cout<<"Movimento para Baixo"<<endl;
+ if(MatrizP.MovimentaBaixo(MatrizDeInsercao.Matriz)){
      if(!Lista->existe(MatrizDeInsercao.Matriz)){
-       if(!MatrizP.VerificaResultado()){
+       cout<<"Movimento para Baixo"<<endl;
+       if(!MatrizDeInsercao.VerificaResultado()){
          Lista->inserir_final(MatrizDeInsercao.Matriz);
        }
        else{
@@ -193,10 +53,10 @@ void Arvore::Insere(MatrizPuzzle MatrizP, Pont *p,Lista* Lista,int* trava){
        Insere(MatrizDeInsercao,&(*p)->Baixo,Lista,trava);
      }
    }
-   if(MatrizP.MovimentaEsquerda(MatrizDeInsercao.Matriz)){
-     cout<<"Movimento para Esquerda"<<endl;
+  if(MatrizP.MovimentaEsquerda(MatrizDeInsercao.Matriz)){
      if(!Lista->existe(MatrizDeInsercao.Matriz)){
-       if(!MatrizP.VerificaResultado()){
+       cout<<"Movimento para Esquerda"<<endl;
+       if(!MatrizDeInsercao.VerificaResultado()){
          Lista->inserir_final(MatrizDeInsercao.Matriz);
        }
        else{
@@ -209,9 +69,9 @@ void Arvore::Insere(MatrizPuzzle MatrizP, Pont *p,Lista* Lista,int* trava){
      }
    }
    if(MatrizP.MovimentaDireita(MatrizDeInsercao.Matriz)){
-     cout<<"Movimento para Direita"<<endl;
      if(!Lista->existe(MatrizDeInsercao.Matriz)){
-       if(!MatrizP.VerificaResultado()){
+       cout<<"Movimento para Direita"<<endl;
+       if(!MatrizDeInsercao.VerificaResultado()){
          Lista->inserir_final(MatrizDeInsercao.Matriz);
        }
        else{
--- main.cpp
+++ main.cpp
@@ -4,13 +4,8 @@
 int main(int argc, char const *argv[]) {
   srand(time(NULL));
   MatrizPuzzle Puzzle;
-  //Puzzle.InicializaMatrizAleatoria();
-  /*int k=1;
-  for(int i=0;i<3;i++)
-    for(int j=0;j<3;j++){
-      Puzzle.Matriz[i][j]=k;
-      k++;
-    }*/
+  Puzzle.InicializaMatrizAleatoria();
+  /*
   Puzzle.Matriz[0][0]=1;
   Puzzle.Matriz[0][1]=8;
   Puzzle.Matriz[0][2]=2;
@@ -21,24 +16,17 @@ int main(int argc, char const *argv[]) {
   Puzzle.Matriz[2][1]=4;
   Puzzle.Matriz[2][2]=7;
 
-  /*
-  for(int i=0;i<3;i++){
-   cout << endl;
-    for(int j=0;j<3;j++){
-      cout << Puzzle.MatrizR[i][j] << " ";
-    }
-  }
-  cout << endl;
+Puzzle.Matriz[0][0]=1;
+Puzzle.Matriz[0][1]=5;
+Puzzle.Matriz[0][2]=2;
+Puzzle.Matriz[1][0]=4;
+Puzzle.Matriz[1][1]=0;
+Puzzle.Matriz[1][2]=3;
+Puzzle.Matriz[2][0]=7;
+Puzzle.Matriz[2][1]=8;
+Puzzle.Matriz[2][2]=6;
+*/
 
-  int MatrizTEste[3][3];
-  Puzzle.MovimentaBaixo(MatrizTEste);
-  for(int i=0;i<3;i++){
-    cout<<endl;
-    for(int j=0;j<3;j++){
-      cout<<MatrizTEste[i][j]<<" ";
-    }
-  }
-  cout<<endl;*/
 
   Arvore Arv;
   Lista Lista;
